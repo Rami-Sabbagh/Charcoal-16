d441f499ea7a759f97fe1a02b5541acf11799530
Rami Sabbagh <ramilego4game@gmail.com>
2020-03-28 03:15:19 +0300

    Get the machine alive

--- Charcoal/init.lua
+++ Charcoal/init.lua
@@ -5,11 +5,18 @@ local class = require(libraryPath..".middleclass")
 
 --Require the Charcoal modules
 local Display = require(libraryPath..".display")
+local Processor = require(libraryPath..".processor")
 
 --The Charcoal class
 local Charcoal = class("charcoal.Charcoal")
 
 function Charcoal:initialize()
+    self.processor = Processor()
+
+    local systemImage = assert(io.open(love.filesystem.getWorkingDirectory().."/debug.bin", "rb"))
+    self.processor:loadImage(systemImage)
+    systemImage:close()
+
     self.display = Display {
         width = 256,
         height = 256,
@@ -17,12 +24,27 @@ function Charcoal:initialize()
         columns = 42,
         rows = 28,
 
-        font = love.graphics.newImage(libraryPath:gsub("%.", "/").."/font.png")
+        font = love.graphics.newImage(libraryPath:gsub("%.", "/").."/font.png"),
+
+        memory = self.processor:getMemory(),
+        firstAddress = 0
     }
+
+    --Temporary
+    self.frequency = 1000 --1 MHz
+    self.cycleTime = 1/self.frequency
+    self.clock = 0
 end
 
 function Charcoal:update(dt)
+    self.clock = self.clock + dt
 
+    local cyclesToExecute = math.floor(self.clock/self.cycleTime)
+    self.clock = self.clock - cyclesToExecute*self.cycleTime
+
+    for i=1, cyclesToExecute do
+        self.processor:executeCycle()
+    end
 end
 
 function Charcoal:draw()
