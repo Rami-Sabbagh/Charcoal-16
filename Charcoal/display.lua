403f7540e92512302fac05215d6e2d31c0db17c8
Rami Sabbagh <ramilego4game@gmail.com>
2020-03-28 18:24:39 +0300

    Improve the display API

--- Charcoal/display.lua
+++ Charcoal/display.lua
@@ -6,9 +6,6 @@ local class = require(libraryPath..".middleclass")
 local display = class("charcoal.Display")
 
 function display:initialize(properties)
-    --The resolution of the canvas
-    self.width, self.height = properties.width, properties.height
-
     --The columns and rows of the characters display
     self.columns, self.rows = properties.columns, properties.rows
 
@@ -27,10 +24,8 @@ function display:initialize(properties)
         end
     end
 
-    --The memory table which contains the VRAM
-    self.memory = properties.memory
-    --The first address of the VRAM
-    self.firstAddress = properties.firstAddress
+    --The resolution of the canvas
+    self.width, self.height = self.characterWidth*self.columns, self.characterHeight*self.rows
 
     --The canvas of the display
     self.canvas = love.graphics.newCanvas(self.width, self.height, { dpiscale=1 })
@@ -38,29 +33,31 @@ function display:initialize(properties)
 
     --The tint of the rendered display
     self.tint = {0, 1, 0, 1}
+
+    --Wrap render call
+    self.wrappedRender = function() self:render(self.memory, self.startAddress) end
 end
 
-function display:render()
-    love.graphics.setCanvas(self.canvas)
-
+function display:render(memory, startAddress)
     love.graphics.clear(0,0,0,1)
     love.graphics.setColor(1,1,1,1)
 
     for j=0, self.rows-1 do
         for i=0, self.columns-1 do
-            local characterID = self.memory[self.firstAddress+i+j*self.columns]
+            local characterID = memory[startAddress+i+j*self.columns]
             love.graphics.draw(self.font, self.charactersQuads[characterID], i*self.characterWidth, j*self.characterHeight)
         end
     end
-
-    love.graphics.setCanvas()
 end
 
-function display:draw()
-    self:render()
+function display:draw(x, y, w, h, memory, startAddress)
+    self.memory, self.startAddress = memory, startAddress
+    self.canvas:renderTo(self.wrappedRender)
+
+    local scale = math.floor(math.min(w/self.width, h/self.height))
 
     love.graphics.setColor(self.tint)
-    love.graphics.draw(self.canvas, 0,0, 0, 2,2)
+    love.graphics.draw(self.canvas, x+(w-self.width*scale)/2, y+(h-self.height*scale)/2, 0, scale, scale)
 end
 
 return display
